35282b9793fe7edf1b93d2edb28120571462a978
peijun <peijunnnn@gmail.com>
2024-11-22 16:34:22 +0800

    revise main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style/Partials/_globalRule.scss'
-import App from './App.vue'
+import '@/style/Partials/_globalRule.scss'
+import App from '@/App.vue'
 
 createApp(App).mount('#app')
